7fe8591f77bec25626418daed8fdb844d7086f80
Jemthestoner <53895473+Jemthestoner@users.noreply.github.com>
2021-06-24 10:00:03 +0800

    update js

--- app.js
+++ app.js
@@ -120,26 +120,6 @@ function logoEvent(){
   
 }
 ///////////////////////////////////////////////////////////////////////////
-fucntion overallProgressBar (){
-    var i = 0;
-    function move(){
-        if (i == 0) {
-            i = 1;
-            var elem = document.getElementById("myBar");
-            var width = 1;
-            var id = setInterval(frame, 10);
-            funtion frame() {
-                if (width >= 100) {
-                    clearInterval(id);
-                    i = 0;
-                } else {
-                    width++;
-                    elem.style.width = width + "%";
-                }
-            }
-            }
-        }
-}
 
 ///////////////////////////////////////////////////////////////////////////
 
